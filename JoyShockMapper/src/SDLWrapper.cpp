44091ecf66f3017f26e8a5914146357b38bbcdf3
ceski <56656010+ceski-1@users.noreply.github.com>
2026-04-03 23:23:26 -0700

    Add JS_TYPE_UNKNOWN

--- JoyShockMapper/src/SDLWrapper.cpp
+++ JoyShockMapper/src/SDLWrapper.cpp
@@ -221,7 +221,7 @@ public:
 	bool _has_gyro;
 	bool _has_accel;
 	int _split_type = JS_SPLIT_TYPE_FULL;
-	int _ctrlr_type = 0;
+	int _ctrlr_type = JS_TYPE_UNKNOWN;
 	uint16_t _small_rumble = 0;
 	uint16_t _big_rumble = 0;
 	AdaptiveTriggerSetting _leftTriggerEffect;
